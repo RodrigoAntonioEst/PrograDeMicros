3ca4af8a2cd232e37c8cfc886e3e04883feb0a25
RodrigoAntonioEst <est23357@uvg.edu.gt>
2025-02-08 15:55:58 -0600

    diseño de lab 1
    Diseño del codigo previo al post lab

--- PRELAB1RODRIGOESTRADA/PRUEBASFINALES/main.asm
+++ PRELAB1RODRIGOESTRADA/PRUEBASFINALES/main.asm
@@ -18,10 +18,10 @@
 
  // Configuración de MCU
  SETUP:
-	LDI R16, 0xF7
-	OUT DDRD, R16 //Configuramos el PORTD como entrada en el bit 3 y en las demas salidas
-	LDI R16, 0X08
-	OUT PORTD, R16 //Apagamos los led en todos los bit menos en el 3 
+	LDI R16, 0XFF 
+	OUT DDRD, R16 //Establecemos el PORTD como salida
+	LDI R16, 0X00
+	OUT PORTD, R16 //Apagamos todos los leds
 
     LDI R16, 0x00 
     OUT DDRB, R16  // PORTB como entrada
@@ -34,25 +34,51 @@
     OUT PORTC, R16 //Desactivamos los leds 
 
     LDI R17, 0xFF  //Estado inicial de botones (todos en pull-up)
-    LDI R19, 0x00  //Registro de conteo
+    LDI R19, 0x00  //Registro de conteo1
+	LDI R20, 0X00 //Registro de conteo2
 
  // Loop principal
 LOOP:
-    IN R16, PINB  //hacemos una lectura en los botones
+	/*IN R16, PINB  //hacemos una lectura en los botones
     CP R16, R17  //Usamos esta funcion para comparar si lo que esta en R17 y R16 es lo mismo
     BREQ LOOP //si es lo mismo se regresa al loop inicial
 	CALL DELAY
 	IN R16, PINB  //se leen de nuevo los botones, luego de cierta cantidad de tiempo 
     CP R16, R17  //si al hacer la comparacion se detecta que el push sigue en el mismo estado se manda de regreso al loop
     BREQ LOOP	//si es igual manda de regreso al loop
+	MOV R17, R16 //se guarda el nuevo estado*/
+	CALL Antirebote
+    CALL ContadorRojo
+	CALL ContadorAzul
+	RJMP LOOP
+
+//SUB RUTINA DE ANTIREBOTE
+Antirebote:
+	IN R16, PINB  //hacemos una lectura en los botones
+    CP R16, R17  //Usamos esta funcion para comparar si lo que esta en R17 y R16 es lo mismo
+    BREQ Antirebote //si es lo mismo se regresa al loop inicial
+	CALL DELAY
+	IN R16, PINB  //se leen de nuevo los botones, luego de cierta cantidad de tiempo 
+    CP R16, R17  //si al hacer la comparacion se detecta que el push sigue en el mismo estado se manda de regreso al loop
+    BREQ Antirebote	//si es igual manda de regreso al loop
 	MOV R17, R16 //se guarda el nuevo estado
+	RET 
+//Establecemos la subrutina para el contador 2
+ContadorRojo:
+	SBRS R17, 2 //si el bit 2 de R17 esta en "1" este se saltara la siguiente instruccion, esto debido a los pullups, cuando el boton este precionado este mandara un 0
+    INC R20 //se realiza el incremento 
+	SBRS R17, 3   //si el bit 3 de R17 esta en "1" este se saltara la siguiente instruccion, esto debido a los pullups, cuando el boton este precionado este mandara un 0
+    DEC R20 //se realiza el decremento 
+	OUT PORTD, R20 //se mandan los datos 
+    RET
+//Establecemos la subrutina del contador 1
+ContadorAzul:
 	SBRS R17, 0 //si el bit 0 de R17 esta en "1" este se saltara la siguiente instruccion, esto debido a los pullups, cuando el boton este precionado este mandara un 0
     INC R19 //se realiza el incremento 
 	SBRS R17, 1   ////si el bit 1 de R17 esta en "1" este se saltara la siguiente instruccion, esto debido a los pullups, cuando el boton este precionado este mandara un 0
     DEC R19 //se realiza el decremento 
 	OUT PORTC, R19 //se mandan los datos 
-    RJMP LOOP //se regresa al bucle infinito
-
+    RET
 //subrutina de interrupcion 
 DELAY: //usamos la funcion delay para poder evitar rebotes en los push 
 	LDI R18, 0
