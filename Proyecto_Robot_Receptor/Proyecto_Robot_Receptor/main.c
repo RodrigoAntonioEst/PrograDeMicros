ef774ca8ee0b5c823d15db7e32f0c77d33ff889c
RodrigoAntonioEst <est23357@uvg.edu.gt>
2025-09-06 18:40:33 -0600

    Receptor

---
+++ Proyecto_Robot_Receptor/Proyecto_Robot_Receptor/main.c
@@ -0,0 +1,18 @@
+/*
+ * Proyecto_Robot_Receptor.c
+ *
+ * Created: 6/09/2025 18:35:43
+ * Author : rodro
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
